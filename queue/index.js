3945fa6e1fad738b3831539f94dc1e681c8a457b
zhuotuo <zhuotuo@leagsoft.com>
2024-07-09 22:29:23 +0800

    queue

---
+++ queue/index.js
@@ -0,0 +1,9 @@
+const queue = []
+
+// 入队
+queue.push(10)
+queue.push(20)
+
+// 出队
+const item1 = queue.shift()
+const item2 = queue.shift()
--- stack/index.js
+++ stack/index.js
@@ -1,5 +1,9 @@
 const stack = []
+
+// 入栈
 stack.push(10)
 stack.push(20)
+
+// 出栈
 const item1 = stack.pop()
 const item2 = stack.pop()
