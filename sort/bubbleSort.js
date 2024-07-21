6f1047ed338d68bf6b860b6cbbc16993525f22fb
zhuotuo <zhuotuo@leagsoft.com>
2024-07-21 21:59:36 +0800

    bubble sort less loop

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,6 +1,6 @@
 Array.prototype.bubbleSort = function () {
   for (let i = 0; i < this.length - 1; i++) {
-    for (let j = 0; j < this.length - 1; j++) {
+    for (let j = 0; j < this.length - 1 - i; j++) {
       if (this[j] > this[j + 1]) {
         const temp = this[j];
         this[j] = this[j + 1];
